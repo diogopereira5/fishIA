c5bca4693592ee3950de6709a6dc5aad0486c067
Diogo Pereira <diogopereira5.ds@gmail.com>
2021-12-07 10:34:04 -0300

    feat: models

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import { generatePopulation } from "./models/fishes.js";
-import { drawFish } from "./utils/drawFish.js";
+import { generatePopulation, drawFish } from "./models/fishes.js";
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
